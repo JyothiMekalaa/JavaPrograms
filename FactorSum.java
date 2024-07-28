ee36894b70e07ac9e1ae44bbeba163ba82c807e3
Jyothimekala99 <146316691+Jyothimekala99@users.noreply.github.com>
2024-07-28 18:13:20 +0530

    Add files via upload

---
+++ FactorSum.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+    public class FactorSum {
+    public static int sumOfFactor(int n) {
+        int factor, sof = 1;
+        int sqrt = (int) Math.sqrt(n);
+        for (factor = 2; factor <= sqrt; factor++) {
+            if (n % factor == 0) {
+                sof = sof + factor + n / factor;
+            }
+        }
+        if (sqrt * sqrt == n) {
+            sof = sof - sqrt;
+        }
+        return sof;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int number = scanner.nextInt();
+        int sum = sumOfFactor(number);
+        System.out.println("Sum of factors of " + number + " is: " + sum);
+        scanner.close();
+    }
+}
